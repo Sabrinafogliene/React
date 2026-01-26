7905b055f43898f7f686d2f8660c7d44d5b5fa9a
Sabrinafogliene <sabrina.castilho4@gmail.com>
2026-01-26 11:53:12 -0300

    init

--- GitWiki/gitwiki/src/pages/App.js
+++ GitWiki/gitwiki/src/pages/App.js
@@ -12,26 +12,32 @@ function App() {
     const [repos, setRepos] = useState([]);
 
     const handleSearchRepo = async () => {
-        const {data} = await api.get(`repos/${currentRepo}`);
+        try{
+            const {data} = await api.get(`repos/${currentRepo}`);
         
-        if (data.id) {
-            const isExist = repos.find(repo => repo.id === data.id);
-            if(!isExist){
-                setRepos(prev => [...prev, data]);
-                setCurrentRepo('');
+            if (data.id) {
+                const isExist = repos.find(repo => repo.id === data.id);
+                if(!isExist){
+                    setRepos(prev => [...prev, data]);
+                    setCurrentRepo('');
                 return;
-            }
+                }
+                alert('Repositório já adicionado!');
+            } 
+        } catch (error) {
             alert('Repositório não encontrado!');
         }
-        
     }
-
+    const handleRemoveRepo = (id) => {
+        const filteredRepos = repos.filter(repo => repo.id !== id);
+        setRepos(filteredRepos);
+    }
     return (
         <Container>
             <img src={gitLogo} width={72} height={72} alt="GitHub Logo" />
             <Input value={currentRepo} onChange={e => setCurrentRepo(e.target.value)} />
             <Button onClick={handleSearchRepo} />
-            {repos.map(repo => <ItemRepo repo={repo} />)}
+            {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />)}
             
         </Container>
     );
